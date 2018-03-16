39721726f6d4fe85efabfa5a3806b8baea5e83b7
ackerm44 <ackerm44@gmail.com>
2018-03-16 09:25:50 -0400

    Added api routes for past and upcoming launches with model methods to query for past and upcoming launches

--- app/models/launch.rb
+++ app/models/launch.rb
@@ -3,4 +3,13 @@ class Launch < ApplicationRecord
   belongs_to :launchpad
 
   validates :flight_number, uniqueness: true
+
+  def self.past_launches
+    self.all.select {|launch| launch.launch_date < Date.today}
+  end
+
+  def self.upcoming_launches
+    self.all.select {|launch| launch.launch_date > Date.today}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   scope '/api' do
     get :rockets, to: 'rockets#index'
     get :launchpads, to: 'launchpads#index'
+    get :past, to: 'launches#past'
+    get :upcoming, to: 'launches#upcoming'
   end
 
 end
