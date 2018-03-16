767c5978278d4a11cb6c85b28ad80c28562ab448
ackerm44 <ackerm44@gmail.com>
2018-03-16 14:28:55 -0400

    Rendering additional details on LaunchShow, added basic styling in Launchshow.css

--- client/src/components/LaunchShow.js
+++ client/src/components/LaunchShow.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { pastLaunchesFetchData } from '../actions/pastLaunches'
+import '../css/launchShow.css'
 
 class LaunchShow extends Component {
   componentDidMount() {
@@ -9,10 +10,17 @@ class LaunchShow extends Component {
 
   render() {
     return (
-      <div>
-        <h2>Yes</h2>
-        <h2>{this.props.launch.id }</h2>
-        <h2>{this.props.launch.flight_number}</h2>
+      <div className="launchDetail">
+        <h2>Flight Number: {this.props.launch.flight_number}</h2>
+        <h3>Launch Date: {this.props.launch.launch_date}</h3>
+        <h5>Launch Successful: {this.props.launch.launch_success}</h5>
+        <img src={this.props.launch.patch_image} alt="patch_image" height="200"/>
+        <p>Details: {this.props.launch.details}</p>
+        <h5>Rocket: {this.props.launch.rocket_name}</h5>
+        <h5>Launchpad: {this.props.launch.launchpad_name}</h5>
+        <p><a href={this.props.launch.article_link} target="_blank">More Information</a></p>
+        <a href={this.props.launch.video_link} target="_blank">Video of Launch</a>
+
       </div>
     )
   }
